d7cbf4977805b8cb67bd931eb9f5080321e68677
Corey Sather <coreysather@gmail.com>
2018-05-31 17:30:36 -0500

    Refactor JssApi a little to reduce clutter/redundancy.

--- app/src/main/java/net/manicmachine/csather/network/JssApi.java
+++ app/src/main/java/net/manicmachine/csather/network/JssApi.java
@@ -1,6 +1,6 @@
 package net.manicmachine.csather.network;
 
-import android.content.Context;
+import android.arch.lifecycle.LiveData;
 import android.util.Base64;
 import android.util.Log;
 
@@ -15,11 +15,11 @@ import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
-import net.manicmachine.csather.jamfbuddy.Util;
+import net.manicmachine.csather.model.Computer;
+import net.manicmachine.csather.model.Record;
 
 import org.json.JSONObject;
 
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,16 +39,66 @@ public class JssApi {
     private String port;
     private String username;
     private String password;
-    private int statusCode;
     private int xmlCounter = 0;
     private boolean connected = true;
 
     public RequestQueue queue;
 
+    /*
+        Authenticate to the JSS by querying the Users endpoint. This way we can confirm that
+        not only do we have the credentials necessary to authenticate to the JSS, but we can
+        also pull what privileges that user may have.
+
+        Currently we don't do anything with the privileges but this'll be used to provide more
+        granular functionality for the end-user.
+
+     */
+
+    // TODO: Capture privileges from response and use that to create an object to represent the current user.
+
     public void authenticate(final VolleyCallback callback) {
 
         String url = buildApiUrl("accounts/username/" + username);
+        JsonObjectRequest request = JssRequest(url, callback);
 
+        RequestQueueSingleton.add(request);
+    }
+
+    public void populateRecords(final String endPoint, final VolleyCallback callback) {
+
+        String url = buildApiUrl(endPoint);
+        JsonObjectRequest request = JssRequest(url, callback);
+
+        RequestQueueSingleton.add(request);
+    }
+
+//    public LiveData<Computer> getComputer(int recordId) {
+//        String url = buildApiUrl("computers", recordId);
+//
+//
+//    }
+
+    public String buildApiUrl(String endPoint) {
+
+        StringBuilder fullUrl = new StringBuilder();
+
+        fullUrl.append(hostname + ":" + port);
+        fullUrl.append("/JSSResource/" + endPoint);
+
+        return fullUrl.toString();
+
+    }
+
+    public String buildApiUrl(String endPoint, int recordId) {
+        StringBuilder fullUrl = new StringBuilder();
+
+        fullUrl.append(hostname + ":" + port);
+        fullUrl.append("/JSSResource/" + endPoint + "/id/" + recordId);
+
+        return fullUrl.toString();
+    }
+
+    public JsonObjectRequest JssRequest(String url, VolleyCallback callback) {
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET,
                 url,
@@ -104,13 +154,13 @@ public class JssApi {
                                         callback.onError(HTTP_INTERNAL_ERROR);
                                         break;
                                     default:
+                                        callback.onError(HTTP_INTERNAL_ERROR);
                                         break;
                                 }
                             }
                         }
 
                         Log.d(TAG, "Volley Error: " + errorString);
-                        callback.onError(HTTP_INTERNAL_ERROR);
                     }
                 }) {
 
@@ -130,14 +180,6 @@ public class JssApi {
 
                 return headers;
 
-            }
-
-            // Override the default parseNetworkResponse so that I can extract the status code
-            // of the returned response.
-            @Override
-            protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
-                statusCode = response.statusCode;
-                return super.parseNetworkResponse(response);
             }
         };
 
@@ -148,120 +190,7 @@ public class JssApi {
                 2
         ));
 
-        RequestQueueSingleton.add(request);
-    }
-
-    public HashMap<String, String> populate(final String target, final VolleyCallback callback) {
-        HashMap<String, String> jssResponse = new HashMap<>();
-
-        String url = buildApiUrl(target);
-        JsonObjectRequest request = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        xmlCounter = 0;
-                        callback.onSuccess(response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        String errorString;
-
-                        if (error.toString().contains("<?xml") && xmlCounter < 10) {
-
-                            // JSS sent XML instead of JSON... Try again.
-                            xmlCounter += 1;
-                            errorString = "Received XML response instead of JSON. Trying again...";
-                            populate(target, callback);
-
-                        } else if (error.toString().contains("<?xml") && xmlCounter >= 10){
-
-                            xmlCounter = 0;
-                            errorString = "Too many XML responses encountered. Quitting...";
-                            callback.onError(HTTP_INTERNAL_ERROR);
-
-                        } else if (error.getClass().equals(TimeoutError.class)) {
-
-                            // The request timed out while trying to contact the JSS.
-                            callback.onError(HTTP_TIMEOUT);
-
-                        } else {
-
-                            // Handle various HTTP errors.
-                            if (error.networkResponse != null) {
-                                switch (error.networkResponse.statusCode) {
-                                    case HTTP_UNAUTHORIZED:
-                                        callback.onError(HTTP_UNAUTHORIZED);
-                                        break;
-                                    case HTTP_FORBIDDEN:
-                                        callback.onError(HTTP_FORBIDDEN);
-                                        break;
-                                    case HTTP_INTERNAL_ERROR:
-                                        callback.onError(HTTP_INTERNAL_ERROR);
-                                        break;
-                                    default:
-                                        break;
-                                }
-                            }
-                        }
-
-                        Log.d(TAG, "Volley Error: " + error.toString());
-                        callback.onError(HTTP_INTERNAL_ERROR);
-                    }
-                }) {
-
-            // Override getHeaders so that we can add basic authentication to the request.
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-
-                HashMap<String, String> headers = new HashMap<>();
-                String credentials = username + ":" + password;
-                String auth = "Basic "
-                        + Base64.encodeToString(credentials.getBytes(),
-                        Base64.NO_WRAP);
-
-                headers.put("Authorization", auth);
-                headers.put("User-Agent", "Pocket Admin");
-                headers.put("Content-Type", "application/json; charset=utf-8");
-                headers.put("Accept", "application/json; charset=utf-8");
-
-                return headers;
-
-            }
-
-            // Override parseNetworkResponse so that we can reference the status code of the response.
-            @Override
-            protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
-                statusCode = response.statusCode;
-                return super.parseNetworkResponse(response);
-            }
-
-        };
-
-        request.setRetryPolicy(new DefaultRetryPolicy(
-                5000,
-                2,
-                2
-        ));
-
-        RequestQueueSingleton.add(request);
-
-        return jssResponse;
-    }
-
-    public String buildApiUrl(String endPoint) {
-
-        StringBuilder fullUrl = new StringBuilder();
-
-        fullUrl.append(hostname + ":" + port);
-        fullUrl.append("/JSSResource/" + endPoint);
-
-        return fullUrl.toString();
-
+        return request;
     }
 
     public static JssApi get() {
@@ -273,7 +202,6 @@ public class JssApi {
     }
 
     private JssApi() {}
-
 
     public void setHostname(String hostname) {
         this.hostname = hostname;
@@ -299,14 +227,6 @@ public class JssApi {
         this.password = password;
     }
 
-    public int getStatusCode() {
-        return this.statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
     public boolean isConnected() {
         return connected;
     }
@@ -314,4 +234,5 @@ public class JssApi {
     public void setConnected(boolean connected) {
         this.connected = connected;
     }
+
 }
